d44bed65bc7d244fc00e042a37fd0b7487db2381
Niklas Fischer <niklas@niklasfi.de>
2016-08-10 01:37:24 +0200

    completely revamped downloading now pooling downloads in the form of sinks in stations

--- src/multi_thread.cpp
+++ src/multi_thread.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <fstream>
 
+#include <mutex>
 #include <thread>
 #include <vector>
 #include <queue>
@@ -12,90 +13,14 @@
 
 #include <boost/filesystem.hpp>
 
-class DlData_direct {
-public:
-    std::ofstream destination;
-    boost::posix_time::ptime until;
-};
-
-size_t write_callback_direct(char *ptr, size_t size, size_t nmemb, void *userdata){
-    DlData_direct* dlData_direct = static_cast<DlData_direct*>(userdata);
-
-    dlData_direct->destination.write(ptr, size * nmemb);
-
-    return boost::posix_time::microsec_clock::local_time() <= dlData_direct->until ? size * nmemb : 0;
-}
-
-void download_direct(const std::string& url, const std::string& destination, boost::posix_time::ptime until){
-    CURL* easyhandle = curl_easy_init();
-
-    DlData_direct dlData_direct { std::ofstream(destination, std::ofstream::out | std::ofstream::app), until };
-
-    //curl_easy_setopt(easyhandle, CURLOPT_URL, "");
-    curl_easy_setopt(easyhandle, CURLOPT_URL, url.c_str());
-
-    curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, write_callback_direct);
-    curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &dlData_direct);
-
-    CURLcode success = curl_easy_perform(easyhandle);
-    if (success != CURLE_OK && success != CURLE_WRITE_ERROR){
-        std::cout << "[ERR] " << destination << " " << curl_easy_strerror(success) << "\n";
-    }
-
-    curl_easy_cleanup(easyhandle);
-}
-
-size_t write_callback_m3u(char* ptr, size_t size, size_t nmemb, void *userdata){
-    std::string* m3u = static_cast<std::string*>(userdata);
-
-    m3u->append(ptr, size * nmemb);
-
-    return size * nmemb;
-}
-
-void download_m3u(const std::string& url, const std::string& destination, boost::posix_time::ptime until){
-    CURL* easyhandle = curl_easy_init();
-    std::string m3u;
-
-    curl_easy_setopt(easyhandle, CURLOPT_URL, url.c_str());
-
-    curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, write_callback_m3u);
-    curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &m3u);
-
-    CURLcode success = curl_easy_perform(easyhandle);
-    if (success != CURLE_OK && success != CURLE_WRITE_ERROR){
-        std::cout << "[ERR] " << destination << " " << curl_easy_strerror(success) << "\n";
-    }
-
-    curl_easy_cleanup(easyhandle);
-
-    std::cout << "'" << m3u << "'\n";
-
-    size_t begin = 0; size_t end = m3u.find_first_of("\r\n", begin);
-    while(boost::posix_time::microsec_clock::local_time() <= until && begin != std::string::npos){
-        std::string snip = m3u.substr(begin, end != std::string::npos ? end-begin : std::string::npos);
-        snip.erase(snip.find_last_not_of(" \n\r\t")+1);
-
-        if(!snip.empty() && snip.front() != '#'){
-            std::cout << "redirecting to -|" << snip << "|-\n";
-            download_direct(snip, destination, until);
-        }
-
-        begin = end;
-        end = m3u.find_first_of("\r\n", begin);
-    }
-
-}
-
-
-class Programme {
+class ConfigurationProgramme {
 public:
     std::string station;
     std::string name;
     std::shared_ptr<NextFunctor::Base> next;
     boost::posix_time::time_duration duration;
 
-    Programme(const std::string& station, const std::string& name, const std::string& next, const boost::posix_time::time_duration& duration):
+    ConfigurationProgramme(const std::string& station, const std::string& name, const std::string& next, const boost::posix_time::time_duration& duration):
         station(station),
         name(name),
         next(NextFunctor::Base::parse(next)),
@@ -103,25 +28,178 @@ public:
     {}
 };
 
-class Station {
+class Programme {
+public:
+    const size_t station_id;
+    const size_t programme_id;
+    const std::string name;
+    std::shared_ptr<NextFunctor::Base> next;
+    const boost::posix_time::time_duration duration;
+
+    Programme(const size_t station_id, const size_t programme_id, const std::string& name, std::shared_ptr<NextFunctor::Base> next, const boost::posix_time::time_duration duration):
+        station_id(station_id),
+        programme_id(programme_id),
+        name(name),
+        next(next),
+        duration(duration)
+    {}
+};
+
+class ConfigurationStation {
 public:
     std::string name;
     std::string url;
 
-    Station(const std::string& name, const std::string& url):
+    ConfigurationStation(const std::string& name, const std::string& url):
         name(name),
         url(url)
     {}
 };
 
+class Sink{
+    boost::posix_time::ptime valid_until;
+    std::unique_ptr<std::ofstream> destination;
+public:
+    Sink(const boost::posix_time::ptime& valid_until, std::unique_ptr<std::ofstream>&& destination):
+        valid_until(valid_until),
+        destination(std::move(destination))
+    {}
+    Sink(Sink&& sink):
+        valid_until(sink.valid_until),
+        destination(std::move(sink.destination))
+    {}
+    Sink& operator=(Sink&& sink) = default;
+    Sink operator=(const Sink& sink) = delete;
+    friend class Station;
+};
+
+class Station {
+public:
+//const and therefore freely accessible
+    const size_t id;
+    const std::string name;
+    const std::string original_url;
+
+private:
+//owned and (predominantly) managed by the cURL thread
+//make sure to acquire the mutex before accessing `sinks`
+    std::unique_ptr<std::mutex> sinks_mutex;
+    std::vector<Sink> sinks;
+
+public:
+    void spawn_direct(const std::string& url){
+        std::thread(&Station::download_direct_loop, this, url).detach();
+    }
+
+    void spawn_m3u(const std::string& url){
+        std::thread(&Station::download_m3u_loop, this, url).detach();
+    }
+
+    void attach(Sink&& sink){
+        //called by the scheduling thread to notify the Station of a new Sink
+        std::lock_guard<std::mutex> lock(*sinks_mutex);
+        sinks.emplace_back(std::move(sink));
+    }
+    Station(size_t id, const std::string& name, const std::string& original_url):
+        id(id),
+        name(name),
+        original_url(original_url),
+        sinks_mutex(new std::mutex()),
+        sinks()
+    {}
+private:
+    static size_t write_callback_direct(char *ptr, size_t size, size_t nmemb, void *userdata){
+        Station* station = static_cast<Station*>(userdata);
+
+        std::lock_guard<std::mutex> lock(*station->sinks_mutex);
+
+        //erase expired sinks
+        boost::posix_time::ptime now(boost::posix_time::microsec_clock::local_time());
+        station->sinks.erase(std::remove_if(station->sinks.begin(), station->sinks.end(), [&now](const Sink& sink){return sink.valid_until < now;}), station->sinks.end());
+
+        //write received data to all of the sinks
+        for(auto& sink: station->sinks){
+            sink.destination->write(ptr, size * nmemb);
+            sink.destination->flush();
+        }
+
+        return size * nmemb;
+    }
+
+    static size_t write_callback_m3u(char* ptr, size_t size, size_t nmemb, void *userdata){
+        std::string* m3u = static_cast<std::string*>(userdata);
+
+        m3u->append(ptr, size * nmemb);
+
+        return size * nmemb;
+    }
+
+    void download_direct(const std::string& url){
+        CURL* easyhandle = curl_easy_init();
+        curl_easy_setopt(easyhandle, CURLOPT_URL, url.c_str());
+
+        curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, write_callback_direct);
+        curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, this);
+
+        //std::cout << name << " direct download starts\n";
+        CURLcode success = curl_easy_perform(easyhandle);
+        if (success != CURLE_OK && success != CURLE_WRITE_ERROR){
+            std::cout << "[ERR] " << name << " " << curl_easy_strerror(success) << "\n";
+        }
+
+        curl_easy_cleanup(easyhandle);
+    }
+
+    void download_direct_loop(const std::string& url){
+        while(true){
+            download_direct(url);
+        }
+    }
+
+    void download_m3u(const std::string& url){
+        CURL* easyhandle = curl_easy_init();
+        std::string m3u;
+
+        curl_easy_setopt(easyhandle, CURLOPT_URL, url.c_str());
+
+        curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, write_callback_m3u);
+        curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &m3u);
+
+        //std::cout << name << " m3u download starts\n";
+        CURLcode success = curl_easy_perform(easyhandle);
+        if (success != CURLE_OK && success != CURLE_WRITE_ERROR){
+            std::cout << "[ERR] " << name << " " << curl_easy_strerror(success) << "\n";
+        }
+
+        curl_easy_cleanup(easyhandle);
+
+        size_t begin = 0; size_t end = m3u.find_first_of("\r\n", begin);
+        while(begin != std::string::npos){
+            std::string snip = m3u.substr(begin, end != std::string::npos ? end-begin : std::string::npos);
+            snip.erase(snip.find_last_not_of(" \n\r\t")+1);
+
+            if(!snip.empty() && snip.front() != '#'){
+                download_direct(snip);
+            }
+
+            begin = end;
+            end = m3u.find_first_of("\r\n", begin);
+        }
+    }
+
+    void download_m3u_loop(const std::string& url){
+        while(true){
+            download_m3u(url);
+        }
+    }
+};
+
 class Event {
 public:
-    std::string station;
-    std::string programme;
+    size_t programme;
     boost::posix_time::ptime time;
 
-    Event(const std::string& station, const std::string& programme, const boost::posix_time::ptime& time):
-        station(station),
+    Event(size_t programme, const boost::posix_time::ptime& time):
         programme(programme),
         time(time)
     {}
@@ -133,49 +211,51 @@ public:
 };
 
 class Scheduler {
-    std::map<std::string, Station> stations;
-    std::map<std::tuple<std::string, std::string>, Programme> programmes;
+    std::vector<Station> stations;
+    std::vector<Programme> programmes;
+
     std::priority_queue<Event> schedule;
+
+
 public:
-    Scheduler(const std::initializer_list<Station>& stations, const std::initializer_list<Programme>& programmes)
+    Scheduler(const std::initializer_list<ConfigurationStation>& cStations, const std::initializer_list<ConfigurationProgramme>& cProgrammes)
     {
-        for(const auto& station: stations){
-            auto result = this->stations.insert({station.name, station});
-            assert(std::get<1>(result));
+        for(const auto& cStation: cStations){
+            stations.emplace_back(stations.size(), cStation.name, cStation.url);
         }
 
-        for(const auto& programme: programmes){
-            auto result = this->programmes.insert({std::make_tuple(programme.station, programme.name), programme});
-            assert(std::get<1>(result));
+        for(const auto& cProgramme: cProgrammes){
+            auto station_it = std::find_if(stations.begin(), stations.end(), [&cProgramme](const Station& station){return station.name == cProgramme.station;});
+            assert(station_it != stations.end());
+
+            programmes.emplace_back(std::distance(stations.begin(), station_it), programmes.size(), cProgramme.name, cProgramme.next, cProgramme.duration);
         }
     }
 
     void run(){
-        for(auto& val: programmes){
-            Programme& programme = std::get<1>(val);
-            auto when = (*programme.next)(boost::posix_time::microsec_clock::local_time(), false);
-            std::cout << "pushing event " << programme.station << "-" << programme.name << " " << when << "\n";
-            schedule.push(Event(programme.station, programme.name, when));
+        //start the station curl threads
+        for(auto& station: stations){
+            station.spawn_m3u(station.original_url);
+        }
+
+        {
+            boost::posix_time::ptime now(boost::posix_time::microsec_clock::local_time());
+            for(auto& programme: programmes){
+                auto when = (*programme.next)(now, false);
+                schedule.push(Event(programme.programme_id, when));
+            }
         }
 
         while(!schedule.empty()){
-            std::cout << "looping!\n";
-
-            boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
             Event event = schedule.top();
 
-            auto programme_it = programmes.find(std::make_tuple(event.station, event.programme));
-            auto station_it = stations.find(event.station);
+            Programme& programme(programmes.at(event.programme));
+            Station& station(stations.at(programme.station_id));
 
-            assert(programme_it != programmes.end());
-            assert(station_it != stations.end());
-
-            auto& programme = programme_it->second;
-            auto& station = station_it->second;
-
+            boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
             auto diff = (event.time - now);
             if(diff.total_microseconds() > 0){
-                std::cout << "sleeping for " << diff << " for " << event.station << "-" << event.programme << "\n";
+                std::cout << "sleeping for " << diff << " for " << station.name << "-" << programme.name << "\n";
                 std::this_thread::sleep_for(std::chrono::microseconds(diff.total_microseconds()));
             }
 
@@ -184,13 +264,12 @@ public:
             boost::filesystem::path dir(prefixPath);
             boost::filesystem::create_directory(prefixPath);
 
-            std::thread thread(download_m3u, station.url, prefixPath + "/" + programme.station + "-" + programme.name + "-" + boost::posix_time::to_iso_extended_string(event.time), event.time + programme.duration);
-            thread.detach();
+            station.attach(Sink(event.time + programme.duration, std::unique_ptr<std::ofstream>(new std::ofstream(prefixPath + "/" + station.name + "-" + programme.name + "-" + boost::posix_time::to_iso_extended_string(event.time), std::ofstream::out | std::ofstream::app))));
 
             schedule.pop();
 
             auto when = (*programme.next)(event.time, true);
-            schedule.push(Event(programme.station, programme.name, when));
+            schedule.push(Event(event.programme, when));
         }
     }
 };
@@ -202,8 +281,8 @@ int main(){
     Scheduler* scheduler;
 
     {
-        using P = Programme;
-        using S = Station;
+        using P = ConfigurationProgramme;
+        using S = ConfigurationStation;
 
         using boost::posix_time::minutes;
 
@@ -243,6 +322,8 @@ int main(){
             }
         );
     }
+
+
 
     scheduler->run();
 
